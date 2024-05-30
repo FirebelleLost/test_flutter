c2db0fe957a38bdf59ebd0e3f91bfdb9621cb0fd
Brian <labe0287@algonquinlive.com>
2024-05-30 16:28:04 -0400

    week 2 done (except yaml)

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lab3',
+      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Lab3'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -31,114 +31,70 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late TextEditingController _controller;
+  late TextEditingController _pwcontroller;
+  var picture = "images/question-mark.png";
 
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+
+    _pwcontroller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+
+    _pwcontroller.dispose();
+    super.dispose();
+  }
+
+  void buttonClicked() {
+    String password = _pwcontroller.value.text;
+
+    setState(() {
+      if (password == "QWERTY123") {
+        picture = "images/idea.png";
+      } else {
+        picture = "images/stop.png";
+      }
+    });
+  }
+
+  var isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget> [
-          Text("Browse Categories", style: TextStyle(fontSize: 50.0, color: Colors.black),),
-
-          Row( mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text("Not sure exactly which recipe you're looking for? do a search, or dive into our most popular categories", style: TextStyle(fontSize: 20.0),),
-            ],),
-
-          Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text("By Meat", style: TextStyle(fontSize: 30.0),)
-            ],),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Stack( alignment: Alignment.center,
-                  children: <Widget>[
-                    Image.asset("images/beef.jpg", width:150, height: 150),
-                    Text("Beef", style: TextStyle(fontSize: 30.0),)
-                  ]),
-              Stack( alignment: Alignment.center,
-                  children: <Widget>[
-                    Image.asset("images/chicken.jpg", width:150, height: 150),
-                    Text("Chicken", style: TextStyle(fontSize: 30.0),)
-                  ]),
-              Stack( alignment: Alignment.center,
-                  children: <Widget>[
-                    Image.asset("images/pork.jpg", width:150, height: 150),
-                    Text("Pork", style: TextStyle(fontSize: 30.0),)
-                  ]),
-              Stack( alignment: Alignment.center,
-                  children: <Widget>[
-                    Image.asset("images/seafood.jpg", width:150, height: 150),
-                    Text("Seafood", style: TextStyle(fontSize: 30.0),)
-                  ]),
-            ],
-          ),
-
-          Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text("By Course", style: TextStyle(fontSize: 30.0),)
-            ],),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Stack( alignment: Alignment.bottomCenter,
-                  children: <Widget>[
-                    Image.asset("images/mainDish.jpg", width:150, height: 150),
-                    Text("Main Dishes", style: TextStyle(fontSize: 30.0),)
-                  ]),
-              Stack( alignment: Alignment.bottomCenter,
-                  children: <Widget>[
-                    Image.asset("images/saladRexipes.jpg", width:150, height: 150),
-                    Text("Salad Recipes", style: TextStyle(fontSize: 30.0),)
-                  ]),
-              Stack( alignment: Alignment.bottomCenter,
-                  children: <Widget>[
-                    Image.asset("images/sideDish.jpg", width:150, height: 150),
-                    Text("Side Dishes", style: TextStyle(fontSize: 30.0),)
-                  ]),
-              Stack( alignment: Alignment.bottomCenter,
-                  children: <Widget>[
-                    Image.asset("images/Crockpot.jpg", width:150, height: 150),
-                    Text("Crockpot", style: TextStyle(fontSize: 30.0),)
-                  ]),
-            ],
-          ),
-
-          Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text("By Dessert", style: TextStyle(fontSize: 30.0),)
-            ],),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Stack( alignment: Alignment.bottomCenter,
-                  children: <Widget>[
-                    Image.asset("images/icecream.jpg", width:150, height: 150),
-                    Text("Ice Cream", style: TextStyle(fontSize: 30.0),)
-                  ]),
-              Stack( alignment: Alignment.bottomCenter,
-                  children: <Widget>[
-                    Image.asset("images/brownie.jpg", width:150, height: 150),
-                    Text("Brownies", style: TextStyle(fontSize: 30.0),)
-                  ]),
-              Stack( alignment: Alignment.bottomCenter,
-                  children: <Widget>[
-                    Image.asset("images/pie.jpg", width:150, height: 150),
-                    Text("Pies", style: TextStyle(fontSize: 30.0),)
-                  ]),
-              Stack( alignment: Alignment.bottomCenter,
-                  children: <Widget>[
-                    Image.asset("images/cookie.jpg", width:150, height: 150),
-                    Text("Cookies", style: TextStyle(fontSize: 30.0),)
-                  ]),
-            ],
-          )
-
-        ],
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+                controller: _controller,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Login")),
+            TextField(
+                controller: _pwcontroller,
+                obscureText: true,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: "Password")),
+            ElevatedButton(
+              onPressed: buttonClicked,
+              child: const Text("Login"),
+            ),
+            Image.asset(picture, width: 200, height: 200),
+          ],
+        ),
       ),
 
       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-
 }
